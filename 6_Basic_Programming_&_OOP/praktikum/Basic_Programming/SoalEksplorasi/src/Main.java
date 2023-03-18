eebc9711f773c73d56e56cacd321906b9a0208f2
samuel <samuelwnrko@gmail.com>
2023-03-19 04:31:36 +0700

    commit tugas basic programming

---
+++ 6_Basic_Programming_&_OOP/praktikum/Basic_Programming/SoalEksplorasi/src/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import static org.example.data.Encrypt.encrypt;
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(encrypt("SEPULSA OKE")); // COZEVCK YUO
+        System.out.println(encrypt("ALTERRA ACADEMY")); // KVDOBBK KMKNOWI
+        System.out.println(encrypt("INDONESIA")); // SXNYXOCSK
+        System.out.println(encrypt("GOLANG")); // QYVKXQ
+        System.out.println(encrypt("PROGRAMMER")); // ZBYQBKWWOB
+    }
+
+}
--- 6_Basic_Programming_&_OOP/screenshot/readme.md
+++
@@ -1,1 +0,0 @@
-# (6) Basic Programming & OOP
