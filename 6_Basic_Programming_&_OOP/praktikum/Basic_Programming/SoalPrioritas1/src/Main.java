c272548a34de4be3ae3632b22b0097ac8cb8cfb4
samuel <samuelwnrko@gmail.com>
2023-03-19 04:37:56 +0700

    commit tugas basic programming

---
+++ 6_Basic_Programming_&_OOP/praktikum/Basic_Programming/SoalPrioritas1/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //Nomor 1
+        System.out.println("Nomor 1");
+        //Segitiga
+        float alas = 20; //input alas segitiga
+        float tinggi = 25; //input tinggi segitiga
+        double luassegitiga = 0.5 * (alas * tinggi); //rumus luas segitiga
+        System.out.println("Luas Segitiga : " + luassegitiga); //print output luas segitiga
+
+        //Persegi Panjang
+        float panjang = 40; //input panjang dari persegi panjang
+        float lebar = 6; //input lebar persegi panjang
+        double luaspersegipanjang = panjang * lebar; //rumus luas persegi panjang
+        System.out.println("Luas Persegi Panjang : " + luaspersegipanjang); //output luas persegi panjang
+
+        //Lingkaran
+        double jari2 = 7; //input jari-jari lingkaran
+        double luaslingkaran = 3.14 * (jari2 * jari2); //rumus luas lingkaran
+        System.out.println("Luas Lingkaran : " + luaslingkaran); ////output luas lingkaran
+
+        //Nomor 2
+        System.out.println("\nNomor 2");
+        System.out.print("Harga Beli : "); //memasukkan harga beli
+        int hargabeli = input.nextInt();
+        System.out.print("Harga Jual : "); //memasukkan harga jual
+        int hargajual = input.nextInt();
+
+        if (hargabeli < hargajual) {
+            System.out.println("Untung sebesar : " + (hargajual - hargabeli)); //jika harga beli lebih besar dari harga jual
+        } else if (hargabeli > hargajual) {
+            System.out.println("Rugi sebesar : " + (hargajual - hargabeli)); //jika harga jual lebih besar dari harga beli
+        } else {
+            System.out.println("Sama saja"); //jika harga beli setara dengan harga jual
+        }
+    }
+}
---
+++ 6_Basic_Programming_&_OOP/praktikum/Basic_Programming/SoalPrioritas2/src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        drawXYZ(5); //untuk input height awal pada nomor 1
+        printAsterisk(5);//untuk input awal pada segitiga nomor 2
+    }
+    public static void drawXYZ(int n) {
+        System.out.println("Nomor 1");
+        for (int i = 1; i <= n*n; i++) {
+            if (i%3 == 0) {
+                System.out.print("X"); // untuk setiap kelipatan 3
+            } else if (i%2 == 1) {
+                System.out.print("Y"); // untuk setiap bilangan ganjil
+            } else {
+                System.out.print("Z"); // untuk setiap bilangan genap
+            }
+            //setelah kelipatan 5 akan melakukan enter ke baris baru
+            if (i%n == 0) {
+                System.out.println();
+            }
+        }
+    }
+    public static void printAsterisk(int z) {
+        System.out.println("\nNomor 2");
+        for (int i = 1; i <= z; i++) {
+            //looping yang digunakan untuk spasi pada bintang.
+            for (int j = z; j >= i; j--) {
+                System.out.print(" ");
+            }
+
+            //looping yang digunakan untuk bintang setiap baris dan spasi setelah bintang tersebut.
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+            // untuk baris baru pada setiap baris yang sudah selesai
+            System.out.println();
+        }
+    }
+}
